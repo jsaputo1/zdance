ebaff181f82c0b26fbc8265969a6754fa8c95952
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2022-07-25 20:45:05 -0400

    initial code and styling for text image split  component

---
+++ themes/zdance/inc/components/text-image-split.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The text-image split component template file
+ *
+ * Fields:
+ * title         text
+ * text_first    boolean
+ * content       WYSIWYG
+ * image         image
+ *
+ * @package zdance
+ */
+
+$index = get_row_index();
+
+?>
+<div class="component component-text-split" id="<?php echo esc_attr( 'section-id-' . $index ); ?>">
+	<div class="row">
+		<?php if ( get_sub_field( 'title' ) ) : ?>
+			<div class="col-12 title">
+				<h2><?php the_sub_field( 'title' ); ?></h2>
+			</div>
+		<?php endif; ?>
+		<div class="col-md-6 text<?php echo ( ! get_sub_field( 'text_first' ) ) ? esc_attr( ' order-md-last' ) : ''; ?>">
+			<?php the_sub_field( 'content' ); ?>
+		</div>
+		<div class="col-md-6 image">
+			<?php echo wp_get_attachment_image( get_sub_field( 'image' ), 'full' ); ?>
+		</div>
+	</div>
+</div>
