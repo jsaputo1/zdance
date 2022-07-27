f7eec0b88516ca700a8ebca762038e5a624f7da5
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2022-07-27 00:02:32 -0400

    initial code and styling for testimonials

--- themes/zdance/inc/components/testimonials.php
+++ themes/zdance/inc/components/testimonials.php
@@ -17,19 +17,21 @@ $index = get_row_index();
 ?>
 <div class="component component-testimonials" id="<?php echo esc_attr( 'section-id-' . $index ); ?>">
     <?php if ( get_sub_field( 'title') ) : ?>
-	    <div class="banner">	
-		    <h2><?php the_sub_field( 'title' ); ?></h2>
-	    </div>
+        <h2><?php the_sub_field( 'title' ); ?></h2>
     <?php endif; ?>
 	<div class="row">
-		<?php while ( have_rows( 'testimonial' ) ) : the_row(); ?>
-			<div class="col-12 wrapper flex-column">
-                <p class="caption"><?php the_sub_field( 'caption' ); ?></p>
-				<h4 class="name"><?php the_sub_field( 'name' ); ?></h4>
-                <?php if ( get_sub_field( 'title') ) : ?>
-				    <span><?php the_sub_field( 'title' ); ?></span>
-                <?php endif; ?>
-			</div>
-		<?php endwhile; ?>
+        <div class="testimonial-slick">
+            <?php while ( have_rows( 'testimonial' ) ) : the_row(); ?>
+                <div>
+                    <div class="col-lg-6 wrapper mx-2 mb-lg-0 mb-3">
+                        <p class="caption"><?php the_sub_field( 'caption' ); ?></p>
+                        <h4 class="name"><?php the_sub_field( 'name' ); ?></h4>
+                        <?php if ( get_sub_field( 'title') ) : ?>
+                            <span><?php the_sub_field( 'title' ); ?></span>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
 	</div>
 </div>
---
+++ themes/zdance/js/zdance.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($){
+$('.testimonial-slick').slick({
+    dots: true,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    adaptiveHeight: true,
+    infinite: true,
+
+  });
+
+});   
